85961f5fc6b585f6eb30dbe38d2e11e7fd0634b8
nawaf sheikh <33278880+greenm8rix@users.noreply.github.com>
2025-06-01 17:02:17 +0200

    final push

---
+++ project/android/app/src/main/assets/public/assets/browser-B8H7QXCo.js
@@ -0,0 +1,2 @@
+import{b as e}from"./vendor-u5qoJQA0.js";const o=e((function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")})),t=Object.freeze(Object.defineProperty({__proto__:null,default:o},Symbol.toStringTag,{value:"Module"}));export{t as b};
+//# sourceMappingURL=browser-B8H7QXCo.js.map
